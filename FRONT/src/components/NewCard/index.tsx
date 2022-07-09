da465cfe56669af196a8ee1bbf9da848a352ba11
Gabriel Guimaraes <silva.guimaraes@ifood.com.br>
2022-07-09 16:04:23 -0300

    refactoring newCard to enable add card just when title and description was filled

--- FRONT/src/components/NewCard/index.tsx
+++ FRONT/src/components/NewCard/index.tsx
@@ -1,36 +1,57 @@
 import { Button, Card, CardActions, CardContent } from '@mui/material';
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { CardsContext } from '../../providers/cards';
 import { Container, TitleInput, DescriptionInput } from './styles';
 
 const NewCard = () => {
     const  { addNewCard }  = React.useContext(CardsContext);
-    const title = useRef<HTMLInputElement>(null)
-    const description = useRef<HTMLInputElement>(null)
+    const titleInput = useRef<HTMLInputElement>(null)
+    const descriptionInput = useRef<HTMLInputElement>(null)
+
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
 
     const sendNewCardRequest = () => {
         addNewCard({
-            titulo: String(title.current?.value),
-            conteudo: String(description.current?.value),
+            titulo: title,
+            conteudo: description,
             lista: 'ToDo'
         })
-        if(title.current) {
-            title.current.value = ''
-        }
-        if(description.current) {
-            description.current.value = ''
-        }
+        setTitle('')
+        setDescription('')
+        if(titleInput.current) titleInput.current.value = ''
+        if(descriptionInput.current) descriptionInput.current.value = ''
     }
 
     return (
         <Container>
             <Card variant="outlined" sx={{ maxWidth: 300 }}>
                 <CardContent>
-                    <TitleInput fullWidth margin='normal' label="Titulo" variant="standard" inputRef={title} />
-                    <DescriptionInput fullWidth margin='normal' label='Descrição' variant='filled' multiline inputRef={description} />
+                    <TitleInput 
+                        fullWidth 
+                        margin='normal' 
+                        label="Titulo" 
+                        variant="standard" 
+                        inputRef={titleInput}
+                        onChange={(event) => setTitle(event.target.value)} />
+                    <DescriptionInput 
+                        fullWidth 
+                        margin='normal' 
+                        label='Descrição' 
+                        variant='filled' 
+                        multiline 
+                        inputRef={descriptionInput}
+                        onChange={(event) => setDescription(event.target.value)}
+                        />
                 </CardContent>
                 <CardActions>
-                    <Button variant="contained" onClick={sendNewCardRequest}>Salvar</Button>
+                    <Button 
+                        variant="contained" 
+                        onClick={sendNewCardRequest}
+                        disabled={( title === '' || description === '' )}
+                        >
+                            Salvar
+                    </Button>
                 </CardActions>
             </Card>
         </Container>
